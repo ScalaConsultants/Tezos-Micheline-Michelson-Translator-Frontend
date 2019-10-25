1a815125340e90cac58a2644ccd79cf75eebc502
Mariusz Siwek <lordh@wp.pl>
2019-10-25 15:44:14 +0200

    Translator - useEffect improvements / fix

--- src/components/translator/Translator.tsx
+++ src/components/translator/Translator.tsx
@@ -1,4 +1,4 @@
-import React, {useCallback, useEffect, useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import { useDispatch, useMappedState } from 'redux-react-hook';
 import * as TranslatorActions from "../../store/actions/translator";
 import * as TranslatorTypes from "./Translator.types";
@@ -16,6 +16,22 @@ const Translator = () => {
   const [micheline, setMicheline] = useState(translator.micheline);
   const [michelson, setMichelson] = useState(translator.michelson);
 
+  const translate = (currentMode: TranslatorTypes.Modes, value: string) => {
+    if(currentMode === TranslatorTypes.Modes.MICHELINEMICHELSON && value.trim().length) {
+      dispatch({
+        type: TranslatorActions.TRANSLATOR_FETCH_MICHELINE_TO_MICHELSON,
+        payload: value
+      });
+    }
+
+    else if(currentMode === TranslatorTypes.Modes.MICHELSONMICHELINE && value.trim().length) {
+      dispatch({
+        type: TranslatorActions.TRANSLATOR_FETCH_MICHELSON_TO_MICHELINE,
+        payload: value
+      });
+    }
+  };
+
   useEffect(() => {
     setMichelson(translator.michelson);
   }, [translator.michelson]);
@@ -23,31 +39,6 @@ const Translator = () => {
   useEffect(() => {
     setMicheline(translator.micheline);
   }, [translator.micheline]);
-
-  const translateCallback = useCallback(() => {
-    if(currentMode === TranslatorTypes.Modes.MICHELINEMICHELSON && micheline.trim().length) {
-      dispatch({
-        type: TranslatorActions.TRANSLATOR_FETCH_MICHELINE_TO_MICHELSON,
-        payload: micheline
-      });
-    }
-
-    else if(currentMode === TranslatorTypes.Modes.MICHELSONMICHELINE && michelson.trim().length) {
-      dispatch({
-        type: TranslatorActions.TRANSLATOR_FETCH_MICHELSON_TO_MICHELINE,
-        payload: michelson
-      });
-    }
-  }, [currentMode, dispatch, micheline, michelson]);
-
-  useEffect(() => {
-    if(micheline.trim().length) translateCallback();
-  }, [micheline, translateCallback]);
-
-
-  useEffect(() => {
-    if(michelson.trim().length) translateCallback();
-  }, [michelson, translateCallback]);
 
   const switchMode = (value?: TranslatorTypes.Modes) => {
     if(value) setCurrentMode(value);
@@ -64,6 +55,8 @@ const Translator = () => {
       type: TranslatorActions.TRANSLATOR_SET_MICHELINE,
       translation: value
     });
+
+    translate(currentMode, value);
   };
 
   const reduxSetMichelson = (value: string) => {
@@ -73,13 +66,21 @@ const Translator = () => {
       type: TranslatorActions.TRANSLATOR_SET_MICHELSON,
       translation: value
     });
+
+    translate(currentMode, value);
+  };
+
+  const getValue = (mode: TranslatorTypes.Modes) => {
+    if(mode === TranslatorTypes.Modes.MICHELINEMICHELSON) return micheline;
+    else if(mode === TranslatorTypes.Modes.MICHELSONMICHELINE) return michelson;
+    else return '';
   };
 
   return (
     <div className="Translator">
       <div className="Translator__header">
         <button className={currentMode === TranslatorTypes.Modes.MICHELINEMICHELSON ? 'Translator__header-selected' : ''} onClick={() => switchMode(TranslatorTypes.Modes.MICHELINEMICHELSON)}>Micheline</button>
-        <img src="arrows.svg" alt="" className="Translator__header__translate-button" />
+        <img src="arrows.svg" alt="" className="Translator__header__translate-button" onClick={() => translate(currentMode, getValue(currentMode))} />
         <button className={currentMode === TranslatorTypes.Modes.MICHELSONMICHELINE ? 'Translator__header-selected' : ''} onClick={() => switchMode(TranslatorTypes.Modes.MICHELSONMICHELINE)}>Michelson</button>
       </div>
       <div className="Translator__translator-area">
--- src/router/routes.tsx
+++ src/router/routes.tsx
@@ -1,14 +1,11 @@
 import React from "react";
 import {Route} from "react-router";
-import Home from "../containers/home/Home";
 import Translation from "../containers/translation/Translation";
 
 export default () => {
-
   let routes =
     <div className="Main">
-      <Route exact path="/" component={Home} />
-      <Route exact path="/translation" component={Translation} />
+      <Route exact path="/" component={Translation} />
     </div>;
 
   return (
