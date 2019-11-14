3847ba119f7e2dab46ef962cad3d5793765c0fcd
Mariusz Siwek <lordh@wp.pl>
2019-11-14 14:29:48 +0100

    Add enzyme

--- src/components/addTranslation/AddTranslation.spec.js
+++
@@ -1,9 +0,0 @@
-import { mount } from 'enzyme';
-import AddTranslation from './AddTranslation';
-
-describe('My Test Suite', () => {
-    it('My Test Case', () => {
-        const comp = mount(<AddTranslation />)
-        expect(true).toEqual(true);
-    });
-});
