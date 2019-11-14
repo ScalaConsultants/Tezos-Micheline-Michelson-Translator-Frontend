cd33d4e656c1185ee8aa90bcadffe06d61d9f94c
Press Play Blog <tabuplaw@gmail.com>
2019-11-14 01:01:26 +0100

    jest working, enzyme not

---
+++ src/components/addTranslation/AddTranslation.spec.js
@@ -0,0 +1,9 @@
+import { mount } from 'enzyme';
+import AddTranslation from './AddTranslation';
+
+describe('My Test Suite', () => {
+    it('My Test Case', () => {
+        const comp = mount(<AddTranslation />)
+        expect(true).toEqual(true);
+    });
+});
