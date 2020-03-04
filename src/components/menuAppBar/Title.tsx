b7852e7544bc64622b1cfc030469e98306a86161
Dariusz Sikorra <dariuszsikorra@MacBook-Pro-Dariusz.local>
2020-03-04 10:58:21 +0100

    MenuAppBar: -change of background color, -change of app name

--- src/components/menuAppBar/Title.tsx
+++ src/components/menuAppBar/Title.tsx
@@ -7,7 +7,7 @@ const Title = () => {
     <div className="Title">
       <div className="Title__l1">Micheline</div>
       <div className="Title__l2">Michelson</div>
-      <div className="Title__l3">Converter v{packageJson.version}</div>
+      <div className="Title__l3">Translator</div>
     </div>
   );
 };
